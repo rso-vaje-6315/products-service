11217c8a9fbb9295156312ef3d461850e742ef2a
Matej <mb7628@student.uni-lj.si>
2019-12-02 18:31:42 +0100

    bugfix popravljeni mapperji, ker sem pozabil premapirat id in timestamp

--- services/src/main/java/si/rso/products/mappers/CategoryMapper.java
+++ services/src/main/java/si/rso/products/mappers/CategoryMapper.java
@@ -6,6 +6,8 @@ import si.rso.products.persistence.CategoryEntity;
 public class CategoryMapper {
     public static Category fromCategoryEntity(CategoryEntity entity) {
         Category category = new Category();
+        category.setId(entity.getId());
+        category.setTimestamp(entity.getTimestamp());
         category.setName(entity.getName());
 
         if (entity.getParentCategory() != null) {
--- services/src/main/java/si/rso/products/mappers/ProductMapper.java
+++ services/src/main/java/si/rso/products/mappers/ProductMapper.java
@@ -7,6 +7,8 @@ public class ProductMapper {
 
     public static Product fromProductEntity(ProductEntity entity) {
         Product product = new Product();
+        product.setId(entity.getId());
+        product.setTimestamp(entity.getTimestamp());
         product.setName(entity.getName());
         product.setDescription(entity.getDescription());
         product.setPrice(entity.getPrice());
